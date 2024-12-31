53472360f240244679665318772d5ddb05d9df5f
NarendraDhamecha <nikhildhamecha19@gmail.com>
2024-12-31 23:10:06 +0530

    converted into plain react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,17 @@ import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import Routing from "./Routing";
 import { ToastContainer } from "react-toastify";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 function App() {
   return (
-    <BrowserRouter>
-      <ToastContainer />
-      <Routing />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <ToastContainer />
+        <Routing />
+      </BrowserRouter>
+    </Provider>
   );
 }
 
